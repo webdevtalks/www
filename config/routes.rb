c684fc7aa4568f8e5e6468d401f73c307847acf0
Ignacio Galindo <joigama@gmail.com>
2013-11-19 21:26:21 -0600

    Revert "Add silly endpoint rendering "sendgrid" text"
    This reverts commit d34bd74ee225e193638a8389a8eae2de16acc7e8.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 WDT::Application.routes.draw do
-  # In order to activate our SendGrid account, they need to see the "sendgrid"
-  # word somewhere in our site.
-  # TODO: Let's remove this one later.
-  match 'sendgrid/validate' => 'sendgrid#validate', via: :get
-
   root to: 'home#index'
 end
