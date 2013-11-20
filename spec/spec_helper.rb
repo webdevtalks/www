e277e1815517c886bf91247db48e1ff99330200e
Ignacio Galindo <joigama@gmail.com>
2013-11-20 03:52:58 -0600

    Add capybara + poltergeist for RSpec integration tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require 'capybara/rails'
+require 'capybara/poltergeist'
+
+# Change default javascript driver
+Capybara.javascript_driver = :poltergeist
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
