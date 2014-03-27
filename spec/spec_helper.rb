43017c0e8ce7a1e8cfd52b32fc2ab68c7f0942ae
Ignacio Galindo <joigama@gmail.com>
2014-03-26 21:21:20 -0600

    Add simplecov for test coverage reports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
 require 'capybara/rails'
 require 'capybara/poltergeist'
+
+SimpleCov.start 'rails' if ENV['COVERAGE']
 
 # Change default javascript driver
 Capybara.javascript_driver = :poltergeist
