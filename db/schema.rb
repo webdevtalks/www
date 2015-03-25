9749b9d8f60e888b8c648c3877fa6f0c5f46c94e
Ignacio Galindo <joiggama@gmail.com>
2015-03-24 18:43:39 -0600

    Add description to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324185314) do
+ActiveRecord::Schema.define(version: 20150325002359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150324185314) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "location_id"
+    t.text     "description"
   end
 
   create_table "locations", force: :cascade do |t|
