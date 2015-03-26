424623be90a7aceaa0b360a5cc753bae48e7d8d8
Ignacio Galindo <joiggama@gmail.com>
2015-03-25 20:26:42 -0600

    Add twitter handle to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325232216) do
+ActiveRecord::Schema.define(version: 20150326013933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20150325232216) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "twitter"
   end
 
 end
