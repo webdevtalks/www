767bd460446e7af9aa832494b914bc602344d0dd
Ignacio Galindo <joiggama@gmail.com>
2015-03-25 20:26:18 -0600

    Sort speakers alphabetically

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 
   belongs_to :location
   has_many   :talks
-  has_many   :speakers, through: :talks, source: :speaker
+  has_many   :speakers, ->{ order 'name asc' }, through: :talks, source: :speaker
 
   def self.current
     find_by 'date >= ?', Time.zone.today
