9c89090cbb0b1e43f8152e450e4baf1ab4648c14
Ignacio Galindo <joiggama@gmail.com>
2015-05-04 15:21:48 +0200

    Add ability to manage events

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,54 @@
+class Admin::EventsController < AdminController
+
+  before_action :find_event, only: [:edit, :update]
+
+  def create
+    @event = Event.create event_params
+
+    if @event.persisted?
+      redirect_to admin_events_path
+    else
+      flash[:error] = @event.humanized_errors
+      render :new
+    end
+  end
+
+  def destroy
+    if Event.destroy(params[:id])
+      redirect_to admin_events_path, notice: 'Evento destruído con éxito.'
+    else
+      redirect_to admin_events_path, error: 'Evento no pudo ser destruído.'
+    end
+  end
+
+  def edit
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def update
+    if @event.update_attributes event_params
+      redirect_to admin_events_path, notice: 'Evento actualizado con éxito.'
+    else
+      flash[:error] = @event.humanized_errors
+      render :edit
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit :venue_id, :date, :description, :theme
+  end
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
+
+end
