dc9f0e76c683eba29c00f62efd9bffea244f619d
Ignacio Galindo <joiggama@gmail.com>
2015-06-17 01:51:27 -0500

    Update tracked database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501192650) do
+ActiveRecord::Schema.define(version: 20150506144220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150501192650) do
     t.datetime "updated_at",  null: false
     t.text     "description"
     t.integer  "venue_id"
+    t.integer  "location_id"
   end
 
   create_table "locations", force: :cascade do |t|
