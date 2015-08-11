d1154ef30b408d37d8b4432b27e5b375bbd8feea
Ignacio Galindo <joiggama@gmail.com>
2015-08-11 02:14:21 -0500

    Add maps to admin venues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def render_map(object, options = {})
+    tag :iframe, width:       options[:width]  || 420,
+                 height:      options[:height] || 280,
+                 frameborder: 0,
+                 style:       'border: 0',
+                 src:         "https://www.google.com/maps/embed/v1/place?key=#{ENV['GOOGLE_MAPS_API_KEY']}&q=#{object.address}"
+  end
+
 end
