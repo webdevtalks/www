80d3a141a5e35c76e41bc7f257c75d10388a6321
Victor Velazquez <victor.velazquez@crowdint.com>
2015-08-13 16:33:17 -0500

    Show sponsor randomly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  SPONSORS = {
+      item1: {
+        link: 'http://www.donchambitas.com',
+        img: 'logo_1.jpg'
+      },
+      item2: {
+        link: 'http://www.crowdint.com',
+        img: 'logo_2.jpg'
+      },
+      item3: {
+        link: "#",
+        img: 'logo_3.jpg'
+      },
+      item4: {
+        link: "#",
+        img: 'logo_3.jpg'
+      }
+  }
+
+  def show_sponsor_randomly
+    html = ''
+    make_it_random.each do |key, value|
+      html+= "<li>"
+      html+=  link_to(image_tag("#{value[:img]}"), "#{value[:link]}", height: 85, width: 200)
+      html+= "</li>"
+    end
+    return html.html_safe
+  end
+
+  private
+
+  def make_it_random
+    SPONSORS.to_a.sample(4).to_h
+  end
 end
