c82c52d207e0e3865e4db3df5cd4209343d353ff
Victor Velazquez <victor.velazquez@crowdint.com>
2015-08-13 19:11:18 -0500

    Convert hash to array to do less calculation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,34 @@
 module ApplicationHelper
-  SPONSORS = {
-      item1: {
+  SPONSORS = [
+      {
+        name: 'cocoworking',
         link: 'https://www.facebook.com/CoCoWorkingMx',
         img: 'logo_1.jpg'
       },
-      item2: {
+      {
+        name: 'crowdint',
         link: 'http://www.crowdint.com',
         img: 'logo_2.jpg'
       },
-      item3: {
+      {
+        name: 'donchambitas',
         link: "http://www.donchambitas.com",
         img: 'logo_3.jpg'
       },
-      item4: {
-        link: "#",
+      {
+        name: 'easybroker',
+        link: "https://www.easybroker.com",
         img: 'logo_4.jpg'
       }
-  }
+  ]
 
   def show_sponsors
     html = ''
-    make_it_random.each do |key, value|
+    SPONSORS.sample(4).each do |value|
       html+= "<li>"
       html+=  link_to(image_tag("#{value[:img]}"), "#{value[:link]}", height: 85, width: 200)
       html+= "</li>"
     end
     return html.html_safe
-  end
-
-  private
-
-  def make_it_random
-    SPONSORS.to_a.sample(4).to_h
   end
 end
