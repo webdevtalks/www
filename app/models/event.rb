09f7fa8a24e7bc7d460437eb9e99611be376d02c
Ignacio Galindo <joiggama@gmail.com>
2015-08-15 10:07:02 -0500

    Sort class methods in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,12 +2,13 @@ class Event < ActiveRecord::Base
 
   belongs_to :venue
 
+  delegate :location, to: :venue
+
   has_many   :talks, dependent: :nullify
   has_many   :speakers, ->{ order 'name asc' }, dependent: :nullify, source: :speaker, through: :talks
 
   validates_presence_of :venue, :date
 
-  delegate :location, to: :venue
 
   def self.current
     find_by('date >= ?', Time.zone.today) || last
