5827388d3b6d7f2e90b31fa935cfe17584731167
Ignacio Galindo <joiggama@gmail.com>
2015-08-25 09:55:16 -0500

    Remove google maps legacy code

--- app/assets/javascripts/app/base.js
+++ app/assets/javascripts/app/base.js
@@ -12,7 +12,6 @@
 			wplook.AnimateCssWpl();
 			wplook.OwlSliderWpl();
 			wplook.MainMenuWpl();
-			//wplook.GoogleMapsWpl(); Until maps get back!
 			wplook.DetectiOSWpl();
 		},
 
@@ -136,71 +135,6 @@
 			Modernizr.addTest('appleios', function () {
 				return (Modernizr.ipad || Modernizr.ipod || Modernizr.iphone);
 			});
-		},
-
-		// Google maps
-		GoogleMapsWpl: function () {
-
-			var map;
-			var montreal = new google.maps.LatLng(wplMap.Lat, wplMap.Long);
-
-			var MY_MAPTYPE_ID = 'custom_style';
-			function initialize() {
-				var featureOpts = [
-					{
-						stylers: [
-							{ saturation: -20 },
-							{ lightness: 40 },
-							{ visibility: 'simplified' },
-							{ gamma: 0.8 },
-							{ weight: 0.4 }
-						]
-					},
-					{
-						elementType: 'labels',
-						stylers: [
-							{ visibility: 'on' }
-						]
-					},
-					{
-						featureType: 'water',
-						stylers: [
-							{ color: '#dee8ff' }
-						]
-					}
-				];
-
-				var mapOptions = {
-					zoom: 14,
-					scrollwheel: false,
-					panControl: false,
-					mapTypeControl: false,
-		  			streetViewControl: false,
-					center: new google.maps.LatLng(wplMap.Lat, wplMap.Long),
-					mapTypeControlOptions: {
-						mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
-					},
-					mapTypeId: MY_MAPTYPE_ID
-				};
-
-				map = new google.maps.Map(document.getElementById('js-map'),mapOptions);
-				var image = 'assets/map-marker.png';
-				var myLatLng = new google.maps.LatLng(wplMarker.Lat, wplMarker.Long);
-				var beachMarker = new google.maps.Marker({
-					position: myLatLng,
-					map: map,
-					icon: image
-				});
-				var styledMapOptions = {
-					name: 'Custom Style'
-				};
-
-				var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
-
-				map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
-			}
-
-			google.maps.event.addDomListener(window, 'load', initialize);
 		}
 	};
 
