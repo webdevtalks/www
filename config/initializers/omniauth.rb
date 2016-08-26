e5081a8ea09d82a27e6b6f7637c695aa4afa5f4d
Ignacio Galindo <joiggama@gmail.com>
2016-08-26 10:26:57 +0200

    Configure heroku review apps

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,16 @@
-WDT::Application.config.middleware.use OmniAuth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
 
   configure do |config|
     config.path_prefix = '/admin/auth'
   end
 
-  provider :github,
-           Rails.application.secrets.github_client_id,
-           Rails.application.secrets.github_client_secret,
-           scope: 'user:email,read:org'
+  unless Rails.env.review?
+    provider(
+      :github,
+      Rails.application.secrets.github_client_id,
+      Rails.application.secrets.github_client_secret,
+      scope: 'user:email,read:org'
+    )
+  end
+
 end
