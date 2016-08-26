5b98a9bb0b785cbeeef03c9eb1a2a48d4500f574
Ignacio Galindo <joiggama@gmail.com>
2016-08-26 15:02:14 +0200

    Fix database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,36 +2,47 @@ unless Rails.env.production?
   Faker::Config.locale = 'es'
 
   puts 'Creating location'
-  location = Location.create! city: 'Colima', state: 'Colima', country: 'México'
+  location = Location.create!(
+    city: 'Colima',
+    state: 'Colima',
+    country: 'México'
+  )
 
   puts 'Creating venue'
-  venue = Venue.create! location:    location,
-                        name:        Faker::Company.name,
-                        address:     "#{Faker::Address.street_suffix} #{Faker::Address.street_address}, #{location.name}, #{Faker::Address.postcode}",
-                        description: Faker::Lorem.paragraphs.join
+  venue = Venue.create!(
+    name:        Faker::Company.name,
+    description: Faker::Lorem.paragraphs.join,
+    address:     "#{Faker::Address.street_suffix} #{Faker::Address.street_address}, #{location.name}, #{Faker::Address.postcode}",
+    location:    location
+  )
 
   puts 'Creating event'
-  event = Event.create! venue: venue,
-                        date:  15.days.from_now,
-                        theme: "#{Faker::Hacker.adjective.titleize} #{Faker::Hacker.noun}"
+  event = Event.create!(
+    venue: venue,
+    date:  15.days.from_now,
+    theme: "#{Faker::Hacker.adjective.titleize} #{Faker::Hacker.noun}"
+  )
 
   puts 'Creating speakers'
   speakers = []
 
   4.times do
-    u = User.create! name:              Faker::Name.name,
-                     email:             Faker::Internet.email,
-                     profile_photo_url: Faker::Avatar.image(nil, '200x200', 'jpg').gsub('http', 'https'),
-                     twitter:           Faker::Internet.user_name
-    speakers << u
+    speakers << User.create!(
+      name:      Faker::Name.name,
+      email:     Faker::Internet.email,
+      photo_url: Faker::Avatar.image(nil, '200x200', 'jpg').gsub('http', 'https'),
+      twitter:   Faker::Internet.user_name
+    )
   end
 
   puts 'Creating event talk proposals'
   speakers.each do |speaker|
-    speaker.talks.create! event:       event,
-                          title:       "#{Faker::Hacker.ingverb.titleize} #{Faker::Hacker.adjective} #{Faker::Hacker.noun}",
-                          description: Faker::Lorem.paragraphs.join,
-                          status:      :proposal
+    speaker.talks.create!(
+      title:       "#{Faker::Hacker.ingverb.titleize} #{Faker::Hacker.adjective} #{Faker::Hacker.noun}",
+      description: Faker::Lorem.paragraphs.join,
+      event:       event,
+      status:      :proposal
+    )
   end
 
   puts 'Accepting event talks'
