177274e202168c24c8467bc3fc5f32785300a379
Ignacio Galindo <joiggama@gmail.com>
2016-09-20 07:41:32 +0200

    Refactor bootstrap helper

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,28 +1,33 @@
 module BootstrapHelper
 
-  ALERT_MAPPINGS = {
-    notice:  'info',
-    success: 'success',
-    error:   'danger',
-    alert:   'warning'
+  ALERTS = {
+    classes: %w(alert alert-dismissable),
+    dismiss: {
+      'class'        => 'close',
+      'data-dismiss' => 'alert',
+      'aria-hidden'  => true
+    },
+    mappings: {
+      notice:  'info',
+      success: 'success',
+      error:   'danger',
+      alert:   'warning'
+    },
   }.with_indifferent_access
 
   def render_bootstrap_alerts
     html = ''
 
-    dismiss_button_options = {
-      'class'        => 'close',
-      'data-dismiss' => 'alert',
-      'aria-hidden'  => true
-    }
+    flash.each do |type, message|
+      mapping = ALERTS[:mappings][type]
 
-    dismiss_button = content_tag(:button, raw('&times;'), dismiss_button_options)
+      next if message.blank? || mapping.blank?
 
-    flash.each do |type, alert_message|
-      next if alert_message.blank? || ALERT_MAPPINGS[type].nil?
-      alert_content = dismiss_button + alert_message
-      alert_classes = %W{alert alert-#{ALERT_MAPPINGS[type]} alert-dismissible}.join(' ')
-      html << content_tag(:div, alert_content, class: alert_classes)
+      html << content_tag(
+        :div,
+        content_tag(:button, raw('&times;'), ALERTS[:dismiss]) + message,
+        class: (ALERTS[:classes] << "alert-#{mapping}").join(' ')
+      )
     end
 
     html.html_safe
